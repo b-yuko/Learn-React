d77596270cff9d846b92e517b21eeabe946bfdb0
b-yuko <yuko.banno.oo@gmail.com>
2024-06-08 15:05:30 +0900

    学習環境を追加した

--- src/component/DescribingTheUiPage.tsx
+++ src/component/DescribingTheUiPage.tsx
@@ -1,22 +1,16 @@
-import { Link } from "react-router-dom";
-import "../css/DescribingTheUiPage.css";
+import "../css/DescribingTheUiPage.css"
 
 function Profile() {
   return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
 }
 
-export function DescribingTheUiPage() {
+export default function Gallery() {
   return (
-    <>
-      <Link to="/">Top Page へ戻る</Link>
-      <h1 className="title">UI の記述</h1>
-
       <section>
         <h1>Amazing scientists</h1>
         <Profile />
         <Profile />
         <Profile />
       </section>
-    </>
   );
 }
---
+++ src/component/Header.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { Dispatch, SetStateAction } from "react";
+import "../css/Header.css"
+
+export function Header({currentUrl, setCurrentUrl}:{currentUrl: string, setCurrentUrl: Dispatch<SetStateAction<string>>}) {
+  let returnLinkComponent: JSX.Element
+  if(currentUrl === "/") {
+    returnLinkComponent = <></>
+  } else {
+    returnLinkComponent = <Link to="/" onClick={() => setCurrentUrl("/")}>Top Page へ戻る</Link>
+  }
+
+  let titleComponent: JSX.Element
+  switch (currentUrl) {
+    case "/tic-tac-toe":
+      titleComponent = <h1 className="title-component">チュートリアル：三目並べ</h1>
+      break;
+    case "/describing-the-ui":
+      titleComponent = <h1 className="title-component">UI の記述</h1>
+      break;
+    case "/adding-interactivity":
+      titleComponent = <h1 className="title-component">インタラクティビティの追加</h1>
+      break;
+    case "/managing-state":
+      titleComponent = <h1 className="title-component">state の管理</h1>
+      break;
+    case "/escape-hatches":
+      titleComponent = <h1 className="title-component">避難ハッチ</h1>
+      break;
+    default:
+      titleComponent = <></>
+  }
+
+  return (
+    <>
+      <div className="return-link">{returnLinkComponent}</div>
+      {titleComponent}
+    </>
+  )
+}
